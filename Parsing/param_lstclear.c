4728a49affce3d9d76254e03d087a7eb45575458
Noe Gennaro <ngennaro@z2r12p4.42lyon.fr>
2023-03-07 19:56:27 +0100

    improve parsing

--- Parsing/param_lstclear.c
+++ Parsing/param_lstclear.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   env_lstclear.c                                     :+:      :+:    :+:   */
+/*   param_lstclear.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngennaro <ngennaro@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 17:36:25 by mbrement          #+#    #+#             */
-/*   Updated: 2023/03/01 03:10:22 by ngennaro         ###   ########lyon.fr   */
+/*   Updated: 2023/03/07 19:09:40 by ngennaro         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ void	param_lstclear(t_param **lst)
 		return ;
 	while (*lst)
 	{
+		free((*lst)->content);
 		next = (*lst)->next;
 		free(*lst);
 		*lst = next;
--- deprecated/main.c
+++ deprecated/main.c
@@ -5,12 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngennaro <ngennaro@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/07 19:20:20 by ngennaro          #+#    #+#             */
-/*   Updated: 2023/03/07 19:54:37 by ngennaro         ###   ########lyon.fr   */
+/*   Created: 2023/02/24 14:38:47 by mbrement          #+#    #+#             */
+/*   Updated: 2023/03/07 19:19:33 by ngennaro         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Tools/Libft/libft.h"
 #include "minishell.h"
+
 
 void	print_params(t_param *list)
 {
@@ -42,7 +44,6 @@ int	main(int argc, char **argv, char **envp)
 {
 	size_t	i;
 	size_t	start;
-	int		close;
 	char	*line;
 	char	*new_str;
 	t_param	*param;
@@ -57,10 +58,9 @@ int	main(int argc, char **argv, char **envp)
 	{
 		line = readline("Enter a line: ");
 		i = 0;
-		param = NULL;
+		param = param_lstnew(NULL); //SWAP 
 		while (line[i])
 		{
-			close = 0;
 			while (line[i] == ' ')
 				i++;
 			start = i;
@@ -70,10 +70,7 @@ int	main(int argc, char **argv, char **envp)
 				while (line[i] && line[i] != '\'')
 					i++;
 				if (!line[i] && line[i] != '\'')
-				{
-					close = 1;
-					break;
-				}
+					error_handler(130);
 				new_str = ft_substr(line, start + 1, i - start - 1);
 			}
 			else if (line[i] == '"')
@@ -82,28 +79,84 @@ int	main(int argc, char **argv, char **envp)
 				while (line[i] && line[i] != '"')
 					i++;
 				if (!line[i] && line[i] != '"')
-				{
-					close = 2;
-					break;
-				}
-				new_str = ft_substr(line, start + 1, i - start - 1);
+					error_handler(130);
+				new_str = ft_substr(line, start + 1, i - start - 1); //LEAKS
 			}
 			else
 			{
 				while (line[i + 1] && line[i + 1] != ' ')
 					i++;
-				new_str = ft_substr(line, start, i - start + 1);
+				new_str = ft_substr(line, start, i - start + 1); //LEAKS
 			}
 			i++;
 			new_lst = param_lstnew(new_str);
 			param_lstadd_back(&param, new_lst);
+			param_lstclear(&new_lst);
 		}
-		if (close == 0)
-			exec_core(param, env);
-		if (close == 1)
-			ft_putstr_fd ("Abort, quotes not close\n", 1);
-		if (close == 2)
-			ft_putstr_fd ("Abort, dobble quotes not close\n", 1);
+		exec_core(param, env);
 		param_lstclear(&param);
 	}
 }
+
+
+// void	free_tab(char **str)
+// {
+// 	size_t	i;
+
+// 	i = -1;
+// 	while (str[++i])
+// 	{
+// 		if (str[i])
+// 			free(str[i]);
+// 	}
+// 	free(str);
+// }
+// int	main(int argc, char **argv, char **envp)
+// {
+// 	t_env	*env;
+// 	// t_env	*test;
+// 	char	**str;
+
+// 	(void)argc;
+// 	// (void)argv;
+// 	g_return_value = 0;
+// 	env = get_env(envp);
+// 	// print_env(env);
+// 	// env_add(env, argv[1]);
+// 	// env = env_unset(env, argv[1]);
+// 		// print_env(env);
+// 	// cd(env, argv[1]);
+// 	// env = env_unset(env, "PWD=");
+// 	// env = env_unset(env, "SHLVL=");
+// 	str = ft_split_shell(argv[1]);
+// 	echo(str);
+// 	free_tab(str);
+// 	// printf("%s\n", env_search(env, "PWD=")->content);
+// 	// printf("\n\n");
+// 	// print_export(env);
+// 	// printf("Done\nNonore a dis de mettre du terreau pour que ca marche\n");
+// 	// print_env(env);
+// 	end_of_prog(env, 0);
+// 	// printf("%p || %p", env_search_less(env, "PATH="), env_search(env, "PATH="));
+// 	// env_add(env, "PATH=cookie");
+// 	// end_of_prog(env, 0);
+// 	// (void)env;env
+// 	// test = env_search(env, argv[1]);
+// 	// if (test)
+// 	// 	printf("\n\n%s%s\n\n", test->name, test->content);
+// 	// else
+// 	// 	printf("failed");
+// 	// env_change(argv[1], argv[2], env);
+// 	// test = env_search(env, argv[1]);
+// 	// if (test)
+// 	// 	printf("\n\n%s%s\n\n", test->name, test->content);
+// 	// else
+// 	// 	printf("failed");
+// }
+
+// 	// (void)env;
+// 	// printf("%s", argv[0]);
+// 	// printf("%s", env->next->name);
+// 	// printf("%s\n", env);
+// 	// printf("done, IT WORK BITCH");
+// 	// printf("name : %s\ncontent : %s\n", env->name, env->content);
