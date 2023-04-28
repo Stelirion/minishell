6178797e4e94ab50547f8a06421fd353eaa60d92
root <root@ngennaro>
2023-04-28 14:03:04 +0200

    norm

--- Parsing/token_check.c
+++ Parsing/token_check.c
@@ -6,11 +6,26 @@
 /*   By: ngennaro <ngennaro@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 13:48:31 by ngennaro          #+#    #+#             */
-/*   Updated: 2023/04/28 13:50:53 by ngennaro         ###   ########.fr       */
+/*   Updated: 2023/04/28 13:58:38 by ngennaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+int	format_handler(char *line, int *i, int *pipe, int *redirect)
+{
+	if (line[*i] == '|')
+		(*pipe)++;
+	else if ((line[*i] == '>' || line[*i] == '<'))
+	{
+		if (line[(*i) + 1] && line[(*i) + 1] == line[*i])
+			(*i)++;
+		(*redirect)++;
+	}
+	else
+		return (0);
+	return (1);
+}
 
 int	token_format(char *line)
 {
@@ -28,14 +43,8 @@ int	token_format(char *line)
 		type = get_status(line[i], type);
 		if (pipe > 1 || redirect > 1)
 			return (0);
-		else if (line[i] == '|' && type == 0)
-			pipe++;
-		else if ((line[i] == '>' || line[i] == '<') && type == 0)
-		{
-			if (line[i + 1] && line[i + 1] == line[i])
-				i++;
-			redirect++;
-		}
+		else if (type == 0 && format_handler(line, &i, &pipe, &redirect))
+			;
 		else if (ft_isalpha(line[i]))
 		{
 			pipe = 0;
